b5c9dded40f42b4386785fcbf3dfd005ffebccf6
Mulyawan <fr1301@protonmail.com>
2026-09-22 14:47:31 +0700

    chore: add prettier and eslit for nuxt

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,19 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
   css: ['~/assets/style.css'],
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-  ]
+  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/image', '@nuxt/test-utils', '@nuxt/ui'],
+  eslint: {
+    config: {
+      standalone: false // Mengintegrasikan ESLint dengan skrip bawaan Nuxt
+    }
+  }
 })
